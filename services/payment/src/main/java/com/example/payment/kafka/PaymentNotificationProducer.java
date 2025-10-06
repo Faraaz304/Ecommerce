37156d128cc6da6ca72f430d63b14b286a715e76
Faraaz304 <faraazshaikh304@gmail.com>
2025-10-06 15:08:47 +0530

    mailhog testing not complete kafka implementation

--- services/payment/src/main/java/com/example/payment/kafka/PaymentNotificationProducer.java
+++ services/payment/src/main/java/com/example/payment/kafka/PaymentNotificationProducer.java
@@ -1,20 +1,20 @@
-// package com.example.payment.kafka;
+package com.example.payment.kafka;
 
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.kafka.core.KafkaTemplate;
-// import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
-// @Service
-// @RequiredArgsConstructor
-// @Slf4j
-// public class PaymentNotificationProducer {
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentNotificationProducer {
 
-//     private final KafkaTemplate<String, PaymentNotificationRequest> kafkaTemplate;
-//     private static final String TOPIC_NAME = "payment-topic";
+    private final KafkaTemplate<String, PaymentNotificationRequest> kafkaTemplate;
+    private static final String TOPIC_NAME = "payment-topic";
 
-//     public void sendPaymentNotification(PaymentNotificationRequest notificationRequest) {
-//         log.info("Sending payment notification to Kafka topic \"{}\"", TOPIC_NAME);
-//         kafkaTemplate.send(TOPIC_NAME, notificationRequest);
-//     }
-// }
+    public void sendPaymentNotification(PaymentNotificationRequest notificationRequest) {
+        log.info("Sending payment notification to Kafka topic \"{}\"", TOPIC_NAME);
+        kafkaTemplate.send(TOPIC_NAME, notificationRequest);
+    }
+}
