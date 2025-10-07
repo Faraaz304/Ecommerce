2c38625794605af37f90dbaf985f5713f43ca7cc
Faraaz304 <faraazshaikh304@gmail.com>
2025-10-07 12:33:54 +0530

    discovery with services running without config connected

--- services/config_server/src/main/java/com/example/config_server/ConfigServerApplication.java
+++ services/config_server/src/main/java/com/example/config_server/ConfigServerApplication.java
@@ -1,13 +1,17 @@
-package com.example.config_server;
+package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // Enables the Config Server functionality
+@EnableDiscoveryClient // Enables it to register with Eureka
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 
 }
